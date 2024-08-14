5b90f647492a4d3fc5b52e9bfe90abf2397bde52
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-08-14 14:00:24 +0300

    feats : app.routes.ts ayarlandı.

--- frontend/rast-mobile-interview/src/app/app.routes.ts
+++ frontend/rast-mobile-interview/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'anasayfa' },
+    { path: 'anasayfa', component: HomeComponent, title: 'Anasayfa' },
+    { path: 'hakkimizda', component: AboutUsComponent, title: 'Hakkımızda' },
+];
