9a57f3fe0f0f85fbbe0eaa8fc877e2e5275efbb0
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-08-14 22:54:42 +0300

    feats : login sayfası tasarımı tamamlandı. İcons.scss dosyasına yeni ikonlar eklendi.  _var.scss'e yeni renk değişkenleri eklendi, button.scss dosyasına btn-purple:disabled eklendi.

--- frontend/rast-mobile-interview/src/app/pages/login/login.component.ts
+++ frontend/rast-mobile-interview/src/app/pages/login/login.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthRepository } from '../../shared/repositories/auth.repository';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  authRepository = inject(AuthRepository);
+  passwordType: string = 'password';
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)])
+  });
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value.email, this.loginForm.value.password);
+    }
+  }
+
+  showPassword(event: any) {
+    event.preventDefault();
+    this.passwordType = (this.passwordType === 'password') ? 'text' : 'password';
+  }
+
 
 }
