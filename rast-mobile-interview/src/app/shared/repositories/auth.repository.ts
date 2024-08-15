bf0892b8a17ffe5f715ec269a3c76f12078f4cce
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-08-15 13:02:21 +0300

    feats : login sayfasına beni hatırla checkboxu eklendi. Ve kullanıcı giriş yaparken http isteğinde buton disabled olduğunda butona loading spinner eklendi.

--- rast-mobile-interview/src/app/shared/repositories/auth.repository.ts
+++ rast-mobile-interview/src/app/shared/repositories/auth.repository.ts
@@ -1,21 +1,29 @@
 import { Injectable, inject } from "@angular/core";
-import { environment } from "../../../environments/environment";
 import { Router } from "@angular/router";
 import { AuthService } from "../services/auth.service";
+import { ToastType, ToastrService } from "../services/toastr.service";
+import { General } from "../models/generals.model";
+import { environment } from "../../../environments/environment";
+import { AuthData } from "../models/auth.model";
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class AuthRepository {
+    public authLocalStorageToken = `${environment.appVersion}-${environment.APP_KEY}`;
     router = inject(Router);
     authService = inject(AuthService);
+    toastrService = inject(ToastrService);
+
+    rememberMeChecked: boolean = false;
+    loading: boolean = false;
 
 
     Login(email: string, password: string) {
-        console.log(email,password);
+        this.loading = true;
+      
     }
 
-
-
+ 
 }
