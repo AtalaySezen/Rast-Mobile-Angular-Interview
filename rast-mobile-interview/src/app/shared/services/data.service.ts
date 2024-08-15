e7a5f615adff25e1f143d9a7c7e7fdc2ed59e659
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-08-15 20:00:22 +0300

    feats : data.service.ts sosyal medya verilerini id ve pagination ile getirme fonksiyonları eklendi.

--- rast-mobile-interview/src/app/shared/services/data.service.ts
+++ rast-mobile-interview/src/app/shared/services/data.service.ts
@@ -1,10 +1,43 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Auth, General, QueryPageSize } from '../models/generals.model';
+import { AuthData } from '../models/auth.model';
+import { environment } from '../../../environments/environment';
+import { SocialMediaData, SocialMediaModel } from '../models/socialMedia.model';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class DataService {
+  http = inject(HttpClient);
+
+  Login(email: string, password: string): Observable<Auth<AuthData>> {
+    return this.http.post<Auth<AuthData>>(environment.apiUrl + 'auth/login',
+      {
+        email: email,
+        password: password
+      }
+    )
+  }
+
+  GetSocialMedias(): Observable<General<SocialMediaData>> {
+    return this.http.get<General<SocialMediaData>>(environment.apiUrl + 'socialMedia');
+  }
+
+  GetSocialMediaWithID(socialMediaID: string): Observable<General<SocialMediaModel>> {
+    if (socialMediaID) {
+      return this.http.get<General<SocialMediaModel>>(environment.apiUrl + `socialMedia/${socialMediaID}`);
+    } else {
+      return this.http.get<General<SocialMediaModel>>(environment.apiUrl + `socialMedia/`);
+    }
+  }
+
+  GetSocialMediaPagination(parameters: QueryPageSize = { page: 1, size: 12 }) {
+    return this.http.get<General<SocialMediaData>>(environment.apiUrl + `socialMedia/?page=${parameters.page}&limit=${parameters.size}`);
+  }
+
 
 
 }
