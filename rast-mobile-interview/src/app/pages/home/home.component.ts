41cc80102660c9820375a24aa96bf98fb8022438
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-08-15 23:27:33 +0300

    feats : anasayfaya'da get, get with id, delete fonksiyonları tamamlandı. Home.repository.ts dosyası tamamlandı. Test amaçlı html üzerinde sosyal media verileri yazdırıldı ve test butonları eklendi.

--- rast-mobile-interview/src/app/pages/home/home.component.ts
+++ rast-mobile-interview/src/app/pages/home/home.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from "../../shared/components/header/header.component";
+import { HomeRepository } from '../../shared/repositories/home.repository';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, CommonModule]
 })
 export class HomeComponent {
+    homeRepository = inject(HomeRepository);
+
+    ngOnInit() {
+        this.getSocialMediaDatas();
+    }
+
+    getSocialMediaDatas() {
+        this.homeRepository.GetSocialMediaDatas();
+    }
+
+    getSocialMediaWithID(id: string) {
+        this.homeRepository.GetSocialMediaWithID(id);
+    }
+
+    deleteSocialMedia(id: string) {
+        const confirmed = confirm('Silmek istediğinize emin misiniz?');
+        if (confirmed) {
+            this.homeRepository.DeleteSocialMediaData(id);
+        }
+    }
+
+
+
 
 }
---
+++ rast-mobile-interview/src/app/shared/repositories/home.repository.ts
@@ -0,0 +1,71 @@
+import { Injectable, inject } from "@angular/core";
+import { ToastType, ToastrService } from "../services/toastr.service";
+import { DataService } from "../services/data.service";
+import { SocialMediaData, SocialMediaModel } from "../models/socialMedia.model";
+import { General } from "../models/generals.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class HomeRepository {
+    DataService = inject(DataService);
+    toastrService = inject(ToastrService);
+
+    socialMediasArray: SocialMediaModel[] = [];
+    page: number = 1;
+    size: number = 4;
+
+    GetSocialMediaDatas() {
+        this.DataService.GetSocialMediaPagination({ page: this.page, size: this.size }).subscribe({
+            next: (data: General<SocialMediaData>) => {
+                if (data.status === 'success' && data.data) {
+                    this.socialMediasArray = data.data.socialMedia;
+                } else {
+                    this.toastrService.show(data.message, ToastType.Error);
+                }
+            },
+            error: (err) => {
+                console.error(err);
+                this.toastrService.show(err.message, ToastType.Error);
+            }
+        });
+    }
+
+    GetSocialMediaWithID(id: string) {
+        this.DataService.GetSocialMediaWithID(id).subscribe({
+            next: (data: General<SocialMediaModel>) => {
+                if (data.status === 'success' && data.data) {
+                    console.log(data.data.name)
+                } else {
+                    this.toastrService.show(data.message, ToastType.Error);
+                }
+            },
+            error: (err) => {
+                console.error(err);
+                this.toastrService.show(err.message, ToastType.Error);
+            }
+        });
+    }
+
+    DeleteSocialMediaData(id: string) {
+        this.DataService.DeleteSocialMedia(id).subscribe({
+            next: (data: General<SocialMediaModel>) => {
+                if (data.status == 'success') {
+                    this.toastrService.show(data.message, ToastType.Success);
+                    this.GetSocialMediaDatas();
+                } else {
+                    this.toastrService.show(data.message, ToastType.Error);
+                }
+            },
+            error: (err) => {
+                console.error(err);
+                this.toastrService.show(err.message, ToastType.Error);
+            }
+        })
+
+    }
+
+
+
+}
