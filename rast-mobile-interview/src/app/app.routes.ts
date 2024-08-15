af97b2ec09de28f7c15fb5f9fa2c258ae03629c7
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-08-15 12:36:32 +0300

    changes : frontend dosya yolları düzenlendi.

--- rast-mobile-interview/src/app/app.routes.ts
+++ rast-mobile-interview/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { LoginComponent } from './pages/login/login.component';
+import { authGuard } from './shared/auth/auth.guard';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'giris' },
-    { path: 'anasayfa', component: HomeComponent, title: 'Anasayfa', canActivate: [] },
-    { path: 'hakkimizda', component: AboutUsComponent, title: 'Hakkımızda' },
+    { path: 'anasayfa', component: HomeComponent, title: 'Anasayfa', canActivate: [authGuard] },
+    { path: 'hakkimizda', component: AboutUsComponent, title: 'Hakkımızda', canActivate: [authGuard] },
     { path: 'giris', component: LoginComponent, title: 'Rast Mobile Angular Interview' },
 
 ];
