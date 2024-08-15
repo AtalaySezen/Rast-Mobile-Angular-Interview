19329b2bc1178bdcd7374b30d99e3386940e08a3
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-08-15 12:48:03 +0300

    feats : auth.service.ts fonksiyonları tamamlandı.

--- rast-mobile-interview/src/app/shared/services/auth.service.ts
+++ rast-mobile-interview/src/app/shared/services/auth.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { General } from '../models/generals.model';
+import { AuthData } from '../models/auth.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  http = inject(HttpClient);
 
-  constructor() { }
+  Login(email: string, password: string): Observable<General<AuthData>> {
+    return this.http.post<General<AuthData>>(environment.apiUrl + 'auth/login',
+      {
+        email: email,
+        password: password
+      }
+    )
+  }
+
+  Register(email: string, password: string): Observable<General<AuthData>> {
+    return this.http.post<General<AuthData>>(environment.apiUrl + 'auth/register',
+      {
+        email: email,
+        password: password
+      }
+    )
+  }
+
+  TokenIsValid(): Observable<General<null>> {
+    return this.http.get<General<null>>(environment.apiUrl + 'token/tokenIsValid');
+  }
+
+
+
 }
+
