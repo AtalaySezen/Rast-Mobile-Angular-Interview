39104f309be89b23b43d947de0a0a53619d9d142
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-08-15 04:05:14 +0300

    feats : header componenti tamamlandı ve projeye eklendi.

--- frontend/rast-mobile-interview/src/app/pages/about-us/about-us.component.ts
+++ frontend/rast-mobile-interview/src/app/pages/about-us/about-us.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../shared/components/header/header.component";
 
 @Component({
-  selector: 'app-about-us',
-  standalone: true,
-  imports: [],
-  templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.scss'
+    selector: 'app-about-us',
+    standalone: true,
+    templateUrl: './about-us.component.html',
+    styleUrl: './about-us.component.scss',
+    imports: [HeaderComponent]
 })
 export class AboutUsComponent {
 
