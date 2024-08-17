898d7b3a0db40dfa38a6190a2030d1250772e8f6
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-08-18 01:54:56 +0300

    feats : tabloya pagination eklendi ve  show rows alanı fonksiyonel hale getirildi.

--- rast-mobile-interview/src/app/shared/components/pagination/pagination.component.ts
+++ rast-mobile-interview/src/app/shared/components/pagination/pagination.component.ts
@@ -1,15 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { HomeRepository } from '../../repositories/home.repository';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pagination',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './pagination.component.html',
   styleUrl: './pagination.component.scss'
 })
 export class PaginationComponent {
   homeRepository = inject(HomeRepository);
+
+  rowValue: number = 4;
+  itemsOptions: number[] = [4, 8];
 
   ngOnDestroy() {
     this.homeRepository.currentPage = 1;
@@ -30,5 +34,25 @@ export class PaginationComponent {
     }
   }
 
+  updateTableRows(): void {
+    this.homeRepository.dataSize = this.rowValue;
+    this.homeRepository.GetSocialMediaDatas();
+  }
+
+  increaseSize() {
+    const currentIndex = this.itemsOptions.indexOf(this.rowValue);
+    if (currentIndex < this.itemsOptions.length - 1) {
+      this.rowValue = this.itemsOptions[currentIndex + 1];
+      this.updateTableRows();
+    }
+  }
+
+  decreaseSize() {
+    const currentIndex = this.itemsOptions.indexOf(this.rowValue);
+    if (currentIndex > 0) {
+      this.rowValue = this.itemsOptions[currentIndex - 1];
+      this.updateTableRows();
+    }
+  }
 
 }
--- rast-mobile-interview/src/app/shared/components/table/table.component.ts
+++ rast-mobile-interview/src/app/shared/components/table/table.component.ts
@@ -1,23 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, SimpleChanges, ViewChild, inject } from '@angular/core';
+import { Component, Input, SimpleChanges, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HomeRepository } from '../../repositories/home.repository';
 import { SocialMediaModel } from '../../models/socialMedia.model';
 import { DialogComponent } from "../dialog/dialog.component";
+import { PaginationComponent } from "../pagination/pagination.component";
 
 @Component({
   selector: 'app-table',
   standalone: true,
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
-  imports: [CommonModule, FormsModule, DialogComponent]
+  imports: [CommonModule, FormsModule, DialogComponent, PaginationComponent]
 })
-
 export class TableComponent {
   @Input() tableData: SocialMediaModel[] = [];
   @ViewChild(DialogComponent) dialogComponent!: DialogComponent;
-  homeRepository = inject(HomeRepository);
 
+  itemsPerPageOptions: number[] = [4, 8, 12];
   dialogIsOpen: boolean = false;
   filterText: string = '';
   columns: { key: string, header: string }[] = [{ key: 'name', header: 'Sosyal Medya Adı' }, { key: 'url', header: 'Sosyal Medya Linki' }, { key: 'description', header: 'Açıklama' }];
@@ -25,12 +24,14 @@ export class TableComponent {
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['tableData'] || changes['columns']) {
-      console.log(this.tableData, this.columns);
+      // console.log(this.tableData, this.columns);
     }
   }
 
   addNewSocialMedia() {
     this.dialogComponent.dialogIsOpen = true;
   }
+
+
 
 }
