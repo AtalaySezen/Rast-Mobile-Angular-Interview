ac4307ee22b5582f338905ce542758eb9ae2e9d2
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-08-18 15:40:39 +0300

    changes : app.config.ts'e hash location strategy eklendi.

--- rast-mobile-interview/src/app/app.config.ts
+++ rast-mobile-interview/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
@@ -7,8 +7,9 @@ import { apiInterceptor } from './shared/auth/api.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-  provideRouter(routes), provideAnimations(), provideHttpClient(
+  provideRouter(routes, withHashLocation()), provideAnimations(), provideHttpClient(
     withInterceptors([apiInterceptor]),
+
   ),
   ]
 };
