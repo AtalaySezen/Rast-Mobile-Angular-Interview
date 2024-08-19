0d10744db1aca7a167198e66b34e7383762d306d
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2024-08-19 23:50:19 +0300

    chore : environment.ts dosyası güncellendi.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     appVersion: 'v1.0.0',
     APP_KEY: 'RastMobile',
-    apiUrl: 'http://my-prod-url'
+    apiUrl: 'https://rast-mobile-angular-interview.vercel.app/api/'
   };
